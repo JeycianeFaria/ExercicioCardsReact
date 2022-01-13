e165e6be81915b8d5f2d828b2b06097609820d3b
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-13 19:14:57 -0300

    feat: Criar componente app

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import react from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => (
+    <div className="App">
+        <h1>Linguagens de Programação</h1>
+    </div>
+);
