4a910213f2e838489386754f81d5c61add241c22
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-14 08:36:53 -0300

    Feat: Criar componente com filhos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import react from "react";
 import "./App.css";
 import Card from "./components/Card";
+import ComFilhos from "./components/ComFilhos";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => (
@@ -9,7 +10,16 @@ export default (props) => (
     <div className="Cards">
       <Card titulo="Card #1">Java</Card>
       <Card titulo="Card #2">Git</Card>
-      <Card titulo="Card #3">Spring Boot</Card>
+      <Card titulo="Card #3">
+        <ComFilhos>
+          <ul>
+            <li>Spring Web</li>
+            <li>Spring Data JPA</li>
+            <li>Spring Validation</li>
+            <li>Spring Security</li>
+          </ul>
+        </ComFilhos>
+      </Card>
       <Card titulo="Card #4">React</Card>
       <Card titulo="Card #5">MariaDB</Card>
     </div>
---
+++ src/components/ComFilhos.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) =>
+    <div>
+        <h2>Spring Boot</h2>
+        <div>
+            {props.children}
+        </div>
+    </div>
