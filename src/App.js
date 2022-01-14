6ca8db8a91cca545477d1e618d0cb7c3740f931a
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-14 08:40:41 -0300

    Feat: Criar componente com parametros

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import react from "react";
 import "./App.css";
 import Card from "./components/Card";
 import ComFilhos from "./components/ComFilhos";
+import ComParametros from "./components/ComParametros";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => (
   <div className="App">
     <h1>Skills</h1>
     <div className="Cards">
-      <Card titulo="Card #1">Java</Card>
+      <Card titulo="Card #1">
+        <ComParametros titulo="Java" subtitulo="Linguagem de programação"/>
+      </Card>
       <Card titulo="Card #2">Git</Card>
       <Card titulo="Card #3">
         <ComFilhos>
---
+++ src/components/ComParametros.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => 
+    <>
+        <h2>{props.titulo}</h2>
+        <p>{props.subtitulo}</p>
+    </>
